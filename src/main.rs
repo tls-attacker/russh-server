84087e113b9c2914357eab4557e0e4b22c8104cd
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2023-02-16 23:04:31 +0100

    feat: Add CLI and config file support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use serde::Deserialize;
 use std::collections::HashMap;
 use std::pin::Pin;
 use std::str::FromStr;
 use std::sync::Arc;
+
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
 
 use futures::FutureExt;
 use russh::server::{Auth, Session};
@@ -9,25 +14,111 @@ use russh::*;
 use russh_keys::*;
 use tokio::sync::Mutex;
 
+use clap::Parser;
+
+/// Simple SSH server written in Rust.
+///
+/// Based on the `echoserver` example:
+///
+/// - <https://github.com/warp-tech/russh/blob/v0.34.0/russh/examples/echoserver.rs>
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Address to bind to.
+    ///
+    /// Overrides the value from the config file.
+    #[arg(short, long)]
+    address: Option<String>,
+
+    /// Port number to listen on.
+    ///
+    /// Overrides the value from the config file.
+    #[arg(short, long)]
+    port: Option<u16>,
+
+    /// Path to configuration file.
+    #[arg(short, long, default_value = "russh-server.toml")]
+    config_file: PathBuf,
+}
+
+fn default_address() -> String {
+    "0.0.0.0".to_string()
+}
+
+fn default_port() -> u16 {
+    22
+}
+
+#[derive(Debug, Deserialize)]
+struct ConfigFile {
+    /// Use a fixed host key.
+    ///
+    /// This is a serialized ed25519 key pair from the `ed25519-dalek` crate. See
+    /// <https://docs.rs/ed25519-dalek/1.0.1/ed25519_dalek/struct.Keypair.html> for details.
+    host_key: Option<PathBuf>,
+
+    /// Address to bind to.
+    #[serde(default = "default_address")]
+    address: String,
+
+    /// Port number to listen on.
+    #[serde(default = "default_port")]
+    port: u16,
+
+    // User configuration.
+    users: HashMap<String, UserConfig>,
+}
+
+#[derive(Debug, Deserialize)]
+struct UserConfig {
+    password: Option<String>,
+
+    #[serde(default)]
+    keys: Vec<String>,
+}
+
 #[tokio::main]
 async fn main() {
     env_logger::builder()
         .filter_level(log::LevelFilter::Debug)
         .init();
 
+    let args = Args::parse();
+    let config_file: ConfigFile =
+        toml::from_str(&std::fs::read_to_string(args.config_file).unwrap()).unwrap();
+
+    let host_key = if let Some(path) = &config_file.host_key {
+        log::debug!("Reading host key from {}...", path.display());
+        let mut f = File::open(path).unwrap();
+        let mut buffer: [u8; 64] = [0; 64];
+        f.read_exact(&mut buffer).unwrap();
+        let kp = ed25519_dalek::Keypair::from_bytes(&buffer).unwrap();
+        russh_keys::key::KeyPair::Ed25519(kp)
+    } else {
+        log::debug!("Generating new host key...");
+        russh_keys::key::KeyPair::generate_ed25519().unwrap()
+    };
+
     let config = Arc::new(russh::server::Config {
         connection_timeout: Some(std::time::Duration::from_secs(3600)),
         auth_rejection_time: std::time::Duration::from_secs(3),
-        keys: vec![russh_keys::key::KeyPair::generate_ed25519().unwrap()],
+        keys: vec![host_key],
         ..Default::default()
     });
     let sh = Server {
         clients: Arc::new(Mutex::new(HashMap::new())),
+        users: Arc::new(config_file.users),
         id: 0,
     };
+    let addr_string = format!(
+        "{}:{}",
+        args.address.unwrap_or(config_file.address),
+        args.port.unwrap_or(config_file.port)
+    );
+    log::info!("Listening on {}...", addr_string);
     russh::server::run(
         config,
-        &std::net::SocketAddr::from_str("0.0.0.0:2222").unwrap(),
+        &std::net::SocketAddr::from_str(&addr_string).unwrap(),
         sh,
     )
     .await
@@ -37,6 +128,7 @@ async fn main() {
 #[derive(Clone)]
 struct Server {
     clients: Arc<Mutex<HashMap<(usize, ChannelId), russh::server::Handle>>>,
+    users: Arc<HashMap<String, UserConfig>>,
     id: usize,
 }
 
@@ -92,8 +184,29 @@ impl server::Handler for Server {
         .boxed()
     }
 
-    fn auth_publickey(self, _: &str, _: &key::PublicKey) -> Self::FutureAuth {
-        self.finished_auth(server::Auth::Accept)
+    fn auth_password(self, user: &str, password: &str) -> Self::FutureAuth {
+        let result = self
+            .users
+            .get(user)
+            .and_then(|userconfig| userconfig.password.as_ref())
+            .filter(|pw| pw == &password)
+            .map(|_| server::Auth::Accept)
+            .unwrap_or_else(|| server::Auth::Reject {
+                proceed_with_methods: Some(MethodSet::all()),
+            });
+        self.finished_auth(result)
+    }
+
+    fn auth_publickey(self, user: &str, k: &key::PublicKey) -> Self::FutureAuth {
+        let result = self
+            .users
+            .get(user)
+            .filter(|userconfig| userconfig.keys.contains(&k.fingerprint()))
+            .map(|_| server::Auth::Accept)
+            .unwrap_or_else(|| server::Auth::Reject {
+                proceed_with_methods: Some(MethodSet::all()),
+            });
+        self.finished_auth(result)
     }
 
     fn data(mut self, channel: ChannelId, data: &[u8], mut session: Session) -> Self::FutureUnit {
